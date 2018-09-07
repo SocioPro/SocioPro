c4666a177303f81997915a4138a9b990dc0a8169
SocioPro <42926905+SocioPro@users.noreply.github.com>
2018-09-07 21:15:40 +0300

    sign up page

---
+++ signUpPage.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Sign Up</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+    <center>
+    <div class="center">
+        <p class="title">Sign Up</p>
+        <input type="text" placeholder="First name" class="sign_up"><br><br>
+        <input type="text" placeholder="Second name" class="sign_up"><br><br>
+        <input type="text" placeholder="User name" class="sign_up"><br><br>
+        <input type="text" placeholder="Email" class="sign_up"><br><br>
+        <input type="text" placeholder="Password" class="sign_up"><br><br>
+        <input type="text" placeholder="Confirm Password" class="sign_up"><br><br>
+        <input  type="submit" value="Sign Up" id="submit_sign_up">
+    
+    </div>
+    </center>
+</html>
